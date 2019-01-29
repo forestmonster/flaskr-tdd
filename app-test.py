de1e8e981afcd55dbf8481ba4f8df6909869860b
Forest Monsen <forest.monsen@gmail.com>
2019-01-29 11:26:28 -0800

    Arguments, not a module

--- app-test.py
+++ app-test.py
@@ -2,6 +2,7 @@ from app import app
 
 import unittest
 
+
 class BasicTestCase(unittest.TestCase):
 
     """The Basic test case."""
@@ -9,9 +10,10 @@ class BasicTestCase(unittest.TestCase):
     def test_index(self):
         """Test the index route. """
         tester = app.test_client(self)
-        response = tester.get('/'.content_type='html/text')
+        response = tester.get("/", content_type="html/text")
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data, b'Hello, World!')
+        self.assertEqual(response.data, b"Hello, World!")
+
 
 if __name__ == "__main__":
     unittest.main()
