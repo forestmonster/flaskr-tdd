ff84eb347a7752bd98a802eaeac4ee1705038108
Forest Monsen <forest.monsen@gmail.com>
2019-01-29 15:55:57 -0800

    Add database methods.

--- app.py
+++ app.py
@@ -23,5 +23,40 @@ PASSWORD = "admin"
 app = Flask(__name__)
 app.config.from_object(__name__)
 
+# Connect to the database.
+def connect_db():
+    """Connects to the database."""
+    rv = sqlite3.connect(app.config["DATABASE"])
+    rv.row_factory = sqlite3.Row
+    return rv
+
+
+# Create the database.
+def init_db():
+    """Creates the database."""
+    with app.app_context():
+        db = get_db()
+        with app.open_resource("schema.sql", mode="r") as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+
+# Open database connection.
+def get_db():
+    """Opens a connection to an existing database."""
+    if not hasattr(g, "sqlite_db"):
+        g.sqlite_db = connect_db()
+    return g.sqlite_db
+
+
+# Close database connection.
+@app.teardown_appcontext
+def close_db(error):
+    """Closes the database connection."""
+    if hasattr(g, "sqlite_db"):
+        g.sqlite_db.close()
+
+
 if __name__ == "__main__":
+    init_db()
     app.run()
