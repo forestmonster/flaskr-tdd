9043420bf1aa725943474fbd26aa6bb837f7652a
Forest Monsen <forest.monsen@gmail.com>
2019-01-29 11:21:28 -0800

    Add test for index route.

---
+++ app-test.py
@@ -0,0 +1,17 @@
+from app import app
+
+import unittest
+
+class BasicTestCase(unittest.TestCase):
+
+    """The Basic test case."""
+
+    def test_index(self):
+        """Test the index route. """
+        tester = app.test_client(self)
+        response = tester.get('/'.content_type='html/text')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data, b'Hello, World!')
+
+if __name__ == "__main__":
+    unittest.main()
