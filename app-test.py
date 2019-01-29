ad033f2f53e03c3ae24080f9f442c386a71d9104
Forest Monsen <forest.monsen@gmail.com>
2019-01-29 11:31:33 -0800

    Test for a 404.

--- app-test.py
+++ app-test.py
@@ -11,8 +11,7 @@ class BasicTestCase(unittest.TestCase):
         """Test the index route. """
         tester = app.test_client(self)
         response = tester.get("/", content_type="html/text")
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data, b"Hello, World!")
+        self.assertEqual(response.status_code, 404)
 
 
 if __name__ == "__main__":
