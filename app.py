5bc817853cb3c035a38408ef1bbbac38492496aa
Forest Monsen <forest.monsen@gmail.com>
2019-01-30 11:45:39 -0800

    Method to add a new post to database.

--- app.py
+++ app.py
@@ -57,6 +57,20 @@ def logout():
     return redirect(url_for("index"))
 
 
+@app.route('/add', methods=['POST'])
+def add_entry():
+    """Add new post to database."""
+    if not session.get('logged_in'):
+        abort(401)
+    db = get_db()
+    db.execute(
+        'INSERT INTO entries (title, text) VALUES (?, ?)',
+        [request.form['title'], request.form['text']]
+    )
+    db.commit()
+    flash('New entry was successfully posted.')
+    return redirect(url_for('index'))
+
 def init_db():
     """Create the database."""
     with app.app_context():
