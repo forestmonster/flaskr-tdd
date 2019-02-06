d9c956f5372c040d71511aff1640164188259d6a
Forest Monsen <forest.monsen@gmail.com>
2019-02-05 21:24:42 -0800

    Use Michael's app.py instead.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-import sqlite3
+import os
+
 from flask import (
     Flask,
     request,
@@ -11,31 +12,56 @@ from flask import (
     flash,
     jsonify,
 )
+from flask_sqlalchemy import SQLAlchemy
 
-# Globals
+
+# get the folder where this file runs
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# configuration
 DATABASE = "flaskr.db"
 DEBUG = True
 SECRET_KEY = "my_precious"
 USERNAME = "admin"
 PASSWORD = "admin"
 
-# Initialize app
+# define the full path for the database
+DATABASE_PATH = os.path.join(basedir, DATABASE)
+
+# database config
+SQLALCHEMY_DATABASE_URI = "sqlite:///" + DATABASE_PATH
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+# create app
 app = Flask(__name__)
 app.config.from_object(__name__)
+db = SQLAlchemy(app)
+
+import models
 
 
 @app.route("/")
 def index():
-    """Select all entries from the database, then display them to the user."""
-    db = get_db()
-    cur = db.execute("SELECT * FROM entries ORDER BY id DESC")
-    entries = cur.fetchall()
+    """Searches the database for entries, then displays them."""
+    entries = db.session.query(models.Flaskr)
     return render_template("index.html", entries=entries)
+
+
+@app.route("/add", methods=["POST"])
+def add_entry():
+    """Adds new post to the database."""
+    if not session.get("logged_in"):
+        abort(401)
+    new_entry = models.Flaskr(request.form["title"], request.form["text"])
+    db.session.add(new_entry)
+    db.session.commit()
+    flash("New entry was successfully posted")
+    return redirect(url_for("index"))
 
 
 @app.route("/login", methods=["GET", "POST"])
 def login():
-    """User login / auth / session management."""
+    """User login/authentication/session management."""
     error = None
     if request.method == "POST":
         if request.form["username"] != app.config["USERNAME"]:
@@ -44,79 +70,33 @@ def login():
             error = "Invalid password"
         else:
             session["logged_in"] = True
-            flash("You were logged in.")
+            flash("You were logged in")
             return redirect(url_for("index"))
     return render_template("login.html", error=error)
 
 
 @app.route("/logout")
 def logout():
-    """User logout / auth / session management."""
+    """User logout/authentication/session management."""
     session.pop("logged_in", None)
     flash("You were logged out")
     return redirect(url_for("index"))
 
 
-@app.route("/add", methods=["POST"])
-def add_entry():
-    """Add new post to database."""
-    if not session.get("logged_in"):
-        abort(401)
-    db = get_db()
-    db.execute(
-        "INSERT INTO entries (title, text) VALUES (?, ?)",
-        [request.form["title"], request.form["text"]],
-    )
-    db.commit()
-    flash("New entry was successfully posted.")
-    return redirect(url_for("index"))
-
-
-@app.route("/delete/<post_id>", methods=["GET"])
+@app.route("/delete/<int:post_id>", methods=["GET"])
 def delete_entry(post_id):
-    """Delete post from database"""
+    """Deletes post from database."""
     result = {"status": 0, "message": "Error"}
     try:
-        db = get_db()
-        db.execute("DELETE FROM entries WHERE id=" + post_id)
-        db.commit()
-        result = {"status": 1, "message": "Post deleted."}
+        new_id = post_id
+        db.session.query(models.Flaskr).filter_by(post_id=new_id).delete()
+        db.session.commit()
+        result = {"status": 1, "message": "Post Deleted"}
+        flash("The entry was deleted.")
     except Exception as e:
         result = {"status": 0, "message": repr(e)}
-
     return jsonify(result)
 
 
-def init_db():
-    """Create the database."""
-    with app.app_context():
-        db = get_db()
-        with app.open_resource("schema.sql", mode="r") as f:
-            db.cursor().executescript(f.read())
-        db.commit()
-
-
-def connect_db():
-    """Connect to an existing database."""
-    rv = sqlite3.connect(app.config["DATABASE"])
-    rv.row_factory = sqlite3.Row
-    return rv
-
-
-def get_db():
-    """Re-use an existing database if present."""
-    if not hasattr(g, "sqlite_db"):
-        g.sqlite_db = connect_db()
-    return g.sqlite_db
-
-
-@app.teardown_appcontext
-def close_db(error):
-    """Close the database connection."""
-    if hasattr(g, "sqlite_db"):
-        g.sqlite_db.close()
-
-
 if __name__ == "__main__":
-    init_db()
     app.run()
